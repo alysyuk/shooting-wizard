85583374c97d576c3b21d365a2b13a8a399b3d38
alysyuk <alysyuk@cogniance.com>
2016-09-25 16:32:32 +0300

    [Configuration] adding more configurational files

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,7 +7,7 @@ var helpers = require('./helpers');
 const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
 
 module.exports = webpackMerge(commonConfig, {
-  devtool: 'source-map',
+  // devtool: 'source-map',
 
   output: {
     path: helpers.root('dist'),
@@ -23,7 +23,11 @@ module.exports = webpackMerge(commonConfig, {
   plugins: [
     new webpack.NoErrorsPlugin(),
     new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
+      mangle: {
+        keep_fnames: true
+      }
+    }),
     new ExtractTextPlugin('[name].[hash].css'),
     new webpack.DefinePlugin({
       'process.env': {
