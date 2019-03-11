f7f9cea41b0ae85f62aed7f0951e71bb08ea5b1c
Andrii Lysiuk <andrii.lysiuk@innovecs.com>
2019-03-11 16:49:41 +0200

    [Bacis layout] prettier + mocks + model + general fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,44 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeyValue } from '@angular/common';
+
+import { from, Observable } from 'rxjs';
+import { groupBy, mergeMap, toArray } from 'rxjs/operators';
 
 import { BaseSceneModel } from '../shared/models/base-scene.model';
-import * as mocks from './mocks.json';
+import * as mocks from '../shared/models/mocks.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   // Temp mock
   public readonly scenes: BaseSceneModel[] = mocks.scenes;
   public sceneSelected: string = '';
   public isSceneEditMode: boolean = false;
 
-  public onTextSelected(textSelected: string) {
+  private fieldsToShowValue: string[] = ['id', 'date_time', 'location'];
+
+  public ngOnInit(): void {
+    // this.scenes.filter();
+    let test: any = null;
+    // test.location: any = null;
+
+    from(mocks.scenes)
+      .pipe(
+        groupBy((scene: BaseSceneModel) => scene.location),
+        mergeMap((scene) => scene.pipe(toArray())),
+      )
+      .subscribe((scenes: BaseSceneModel[]) => {
+        console.log(scenes);
+        // test[scenes[0].location] = scenes;
+      });
+
+    console.log(test);
+  }
+
+  public onTextSelected(textSelected: string): void {
     this.sceneSelected = textSelected;
   }
 
@@ -22,13 +46,13 @@ export class AppComponent {
     this.isSceneEditMode = !this.isSceneEditMode;
   }
 
-  public sortById(a: BaseSceneModel, b: BaseSceneModel): number {
-    return a.id > b.id ? -1 : (b.id > a.id ? 1 : 0);
-  };
-
-  public test(data: any) {
-    debugger;
+  public sortById(a: KeyValue<string, any>, b: KeyValue<string, any>): number {
+    if (a.key === 'id') {
+      return a.key > b.key ? -1 : b.key > a.key ? 1 : 0;
+    }
   }
 
-
+  public isNeededToShowValue(field: string): boolean {
+    return this.fieldsToShowValue.includes(field);
+  }
 }
--- src/shared/models/base-scene.model.ts
+++ src/shared/models/base-scene.model.ts
@@ -1,18 +1,18 @@
 export interface BaseSceneModel {
-  'id': number|string,
-  'date_time': number|string,
-  'location': string,
-  'int': any,
-  'fd': any,
-  'ft': any,
-  'characters': any,
-  'sound_eq': any,
-  'lightning eq': any,
-  'props': any,
-  'scene_props': any,
-  'makeup': any,
-  'dresses': any,
-  'prod_design': any,
-  'extras': any,
-  'vfx': any
+  id: number | string;
+  date_time: number | string;
+  location: string;
+  int: boolean; // interior - exterior
+  fd: any; // fixed date
+  ft: any; // fixed time
+  characters: any; // textarea
+  sound_eq: any; // textarea
+  'lightning eq': any; // textarea
+  props: any; // textarea (реквизит)
+  scene_props: any; // textarea (реквизит интерьера)
+  makeup: any; // add picture to column
+  dresses: any;
+  prod_design: any;
+  extras: any;
+  vfx: any;
 }
